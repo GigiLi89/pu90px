8abfd0df8e2816bbaba9e08514596e7613f231e4
GigiLi89 <li.gigi89@gmail.com>
2024-05-16 10:01:42 +0000

    Added comments section to database

--- album/models.py
+++ album/models.py
@@ -9,7 +9,17 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
+    image = models.ImageField(null=True, blank=True)
     content = models.TextField()
-    image_id = models.ImageField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    comment = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
