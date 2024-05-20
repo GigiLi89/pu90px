17c92e9a2a5b8b119e13afb38a077f7c8aab1f50
GigiLi89 <li.gigi89@gmail.com>
2024-05-20 12:53:22 +0000

    Added another trusted host and console log

--- pu90px/settings.py
+++ pu90px/settings.py
@@ -31,9 +31,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = true
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gigili89-pu90px-5w4wvhtne2p.ws-eu111.gitpod.io',
+                '8000-gigili89-pu90px-5w4wvhtne2p.ws-eu114.gitpod.io',
                 '.herokuapp.com']
 
 
@@ -103,6 +104,7 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-gigili89-pu90px-5w4wvhtne2p.ws-eu111.gitpod.io",
+    "htpps://8000-gigili89-pu90px-5w4wvhtne2p.ws-eu114.gitpod.io",
     "https://*.ws.gitpod.io",
     "https://*.herokuapp.com",
     
