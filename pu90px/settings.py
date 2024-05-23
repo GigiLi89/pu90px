b5ab11cfe64afe4cbf2d178d09fd8dda39b9984b
GigiLi89 <li.gigi89@gmail.com>
2024-05-23 10:50:26 +0000

    Heroku issues

--- pu90px/settings.py
+++ pu90px/settings.py
@@ -96,7 +96,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -166,6 +166,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
